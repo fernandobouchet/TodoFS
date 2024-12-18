c54434fce2e6f9cdb6d60148b05281368bf6306d
fernandobouchet <fernandobouchet@gmail.com>
2024-12-17 21:29:03 -0300

    feat(backend): Initialize project with 'springinitializr' with: * Spring Web * Spring Data JPA * MySQL Driver * Lombok

---
+++ backend/src/main/java/com/app/todoappfs/TodoappfsApplication.java
@@ -0,0 +1,13 @@
+package com.app.todoappfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappfsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/todoappfs/TodoappfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.todoappfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
